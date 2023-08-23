1dc1ae9e606c5676a9cf9fe1efcf565032c39dbd
dreas <dreas269@gmail.com>
2023-08-23 11:59:06 +0700

    update incorect state update logo menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,7 @@ export default function Header() {
         <nav
             className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <ActiveLink href="/" className="flex items-center" onHandleMobileMenu={onHandleMobileMenu}>
+                <ActiveLink href="/" className="flex items-center" onHandleMobileMenu={() => setIsShowMobileMenu(false)}>
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo"/>
                     <span
                         className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>

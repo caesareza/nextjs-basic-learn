86a9af3b86a3288fdbdf84b1a4670ca17780a5d5
dreas <dreas269@gmail.com>
2023-08-28 23:04:39 +0700

    add next analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 import Layout from "@/components/Layout";
 
 export default function LearnNextApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
